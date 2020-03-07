1d3d2b2909db2667eb6a6356965b3c85759c157b
naivomah3 <njato@outlook.fr>
2020-03-08 00:50:57 +0530

    add deeplab helpers

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import math
 import os
 import random
 import string
+from keras.activations import relu
 
 def get_rand_name(size=30, chars=string.ascii_letters + string.digits):
     '''
@@ -27,5 +28,19 @@ def lr_decay(epoch):
     frequency_drop = 5.0
     lr = init_lr * math.pow(drop, math.floor((1 + epoch) / frequency_drop))
     return lr
+
+
+# DeepLab helpers
+def relu6(layer):
+    return relu(layer, max_value=6)
+
+def make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
 
 
