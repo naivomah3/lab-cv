a961fd3770d8bf96ec0645e10db88946a953d77c
naivomah3 <njato@outlook.fr>
2020-03-09 19:05:03 +0530

    add deeplab model

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,7 +1,10 @@
 import os
-from src.models import UNet, BCD_UNet_D1, BCD_UNet_D3, FC_DenseNet, DSC_DeepLab_v3_plus
+from src.models.unet import UNet
+from src.models.bcd_unet import BCD_UNet_D1, BCD_UNet_D3
+from src.models.fc_densenet import FC_DenseNet
+from src.models.deeplab_v3_plus import DSC_DeepLab_v3_plus
 
-WEIGHTS_IN_PATH = os.environ.get("WEIGHTS_IN_PATH") # should be the pre-trained weights full path
+WEIGHTS_IN_PATH = os.environ.get("WEIGHTS_IN_PATH") # should be the pre-trained models_weights full path
 IN_HEIGHT = int(os.environ.get("IN_HEIGHT"))
 IN_WIDTH = int(os.environ.get("IN_WIDTH"))
 LABELS_FILE = os.environ.get("LABELS_FILE")
@@ -34,7 +37,7 @@ MODELS = {
                                     input_h=IN_HEIGHT,
                                     input_w=IN_WIDTH,
                                     model_name=f"{MODEL}"),
-    # Fully Convolutional Network
+    # Fully Convolutional Network: 3 variants
     'fcn_densenet_103': FC_DenseNet(pre_trained=is_pretrained,
                                 weights_path=WEIGHTS_IN_PATH,
                                 n_classes=CLASSES,
@@ -53,7 +56,7 @@ MODELS = {
                                     input_h=IN_HEIGHT,
                                     input_w=IN_WIDTH,
                                     model_name=f"{MODEL}"),
-    # Depthwise Separable Convolution with 2 backbones available: Mobilenetv2 and Xception
+    # DeepLabV3+: Depthwise Separable Convolution with 2 backbones available: Mobilenetv2 and Xception
     'deeplab_v3_plus': DSC_DeepLab_v3_plus(pre_trained=is_pretrained,
                                            weights_path=WEIGHTS_IN_PATH,
                                            n_classes=CLASSES,
