8353c61b8b99cff392c3fdad04470b95add0b32c
naivomah3 <njato@outlook.fr>
2020-04-18 16:05:48 +0530

    add additional model calls

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -3,6 +3,9 @@ from src.models.unet import UNet
 from src.models.bcd_unet import BCD_UNet_D1, BCD_UNet_D3
 from src.models.fc_densenet import FC_DenseNet
 from src.models.deeplab_v3_plus import DSC_DeepLab_v3_plus
+from src.models.fcn_8s import FCN_8s
+from src.models.dense_unet import Dense_UNet
+from src.models.segnet import SegNet
 
 WEIGHTS_IN_PATH = os.environ.get("WEIGHTS_IN_PATH") # should be the pre-trained models_weights full path
 IN_HEIGHT = int(os.environ.get("IN_HEIGHT"))
@@ -24,6 +27,31 @@ MODELS = {
                 input_h=IN_HEIGHT,
                 input_w=IN_WIDTH,
                 model_name=f"{MODEL}"),
+
+    # Fully Convolutional Network
+    'fcn_8s': FCN_8s(pre_trained=is_pretrained,
+                    weights_path=WEIGHTS_IN_PATH,
+                    n_classes=CLASSES,
+                    input_h=IN_HEIGHT,
+                    input_w=IN_WIDTH,
+                    model_name=f"{MODEL}"),
+
+    # Dense-U-Net(proposed model for experiments)
+    'dense_unet': Dense_UNet(pre_trained=is_pretrained,
+                    weights_path=WEIGHTS_IN_PATH,
+                    n_classes=CLASSES,
+                    input_h=IN_HEIGHT,
+                    input_w=IN_WIDTH,
+                    model_name=f"{MODEL}"),
+
+    # SegNet: A deep convolutional encoder-decoder architecture for image segmentation
+    'seg_net': SegNet(pre_trained=is_pretrained,
+                    weights_path=WEIGHTS_IN_PATH,
+                    n_classes=CLASSES,
+                    input_h=IN_HEIGHT,
+                    input_w=IN_WIDTH,
+                    model_name=f"{MODEL}"),
+
     # Bi-Directional ConvLSTM U-Net with Densely Connected Convolutions
     'bcd_unet_d3': BCD_UNet_D3(pre_trained=is_pretrained,
                                 weights_path=WEIGHTS_IN_PATH,
